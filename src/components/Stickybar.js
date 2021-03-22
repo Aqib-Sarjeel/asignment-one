b390caa17ae4405e1d95cc962defef28b246a854
Aqib-Sarjeel <iamaqibsarjeel@gmail.com>
2021-03-22 22:54:33 +0530

    all files

---
+++ src/components/Stickybar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Stickyabar = () => {
+  return (
+    <div className="stickybar">
+      <ul className="list-order">
+        <li>home</li>
+        <li>products</li>
+        <li>reports</li>
+        <li>contacts</li>
+      </ul>
+    </div>
+  );
+};
+export default Stickyabar;
